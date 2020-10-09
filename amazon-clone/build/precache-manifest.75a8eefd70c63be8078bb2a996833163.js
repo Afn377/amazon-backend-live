e9c91d2feac74cc7c1138141694662da6ba5c8fa
unknown <afnanthegamer2018@gmail.com>
2020-10-10 01:35:05 +0600

    fix

---
+++ amazon-clone/build/precache-manifest.75a8eefd70c63be8078bb2a996833163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7895a3f23e690dc7358cd244990f22d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46770ad27a3113f7c427",
+    "url": "/static/css/main.5eeb8cc8.chunk.css"
+  },
+  {
+    "revision": "86a574dab09331c895ea",
+    "url": "/static/js/2.6e6f9160.chunk.js"
+  },
+  {
+    "revision": "890fe60e51f18d9e8e0004117fd20dc3",
+    "url": "/static/js/2.6e6f9160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46770ad27a3113f7c427",
+    "url": "/static/js/main.5d92f8a2.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
